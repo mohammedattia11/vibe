bf250099e131586fa13e6a4137735d6a61432b64
mohamed atia <mohamedd.attia11@gmail.com>
2025-11-30 01:16:24 +0200

    feat:implementing ai-jobs, connecting application with ai

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Home() {
+  const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
-  return <div className="p-4">
-    <Button onClick={()=>invoke.mutate({text :"atia"})} className="cursor-pointer">Invoke Background job</Button>
-  </div>;
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success("Background job has been started");
+        setValue("")
+      },
+    })
+  );
+
+  return (
+    <div className="p-4 flex flex-col gap-5 w-1/5 ">
+      <Input
+        placeholder="Start typing"
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ value: value })}
+        className="cursor-pointer"
+      >
+        Invoke Background job
+      </Button>
+    </div>
+  );
 }
--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,18 @@
+import { createAgent, gemini } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are an expert next.js developer. you write readable, maintainable code. you write simple Next.js & React snippets",
+      model: gemini({ model: "gemini-2.5-flash", apiKey:process.env.GEMINI_API_KEY }),
+    });
+    const { output } =  await codeAgent.run(`Write the following snippet : ${event.data.value}`);
+    console.log(output);
+    return { output };
+  }
 );
