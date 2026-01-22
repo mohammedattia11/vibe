13076676931d7d684b3365a29e216184315d7daf
Youssef Waleed <youssef.waleed22470@ci.menofia.edu.eg>
2026-01-22 03:58:44 +0200

    feat: update prisma client version and add FragmentWeb component with hint tooltips

--- modules/projects/server/ui/views/project-view.tsx
+++ modules/projects/server/ui/views/project-view.tsx
@@ -1,48 +1,48 @@
-"use client" ;
+"use client";
 
 import {
-    ResizableHandle,
-    ResizablePanel,
-    ResizablePanelGroup,
-} from "@/components/ui/resizable" ;
-import { MessagesContainer } from "./components/messages-container" ;
-import { Suspense, useState } from "react" ;
-import { Fragment } from "@/app/generated/prisma" ;
-import { ProjectHeader } from "./components/project-header" ;
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { MessagesContainer } from "./components/messages-container";
+import { Suspense, useState } from "react";
+import { ProjectHeader } from "./components/project-header";
+import { FragmentWeb } from "./components/Fragment-web";
+import type { Fragment } from "./components/Fragment-web";
 
 interface Props {
-    projectId : string ;
-};
+  projectId: string;
+}
 
 export const ProjectView = ({ projectId }: Props) => {
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
 
-    const [activeFragment , setActiveFragment] = useState<Fragment | null>(null) ;
-
-    return (
-        <div className="h-screen">
-            <ResizablePanelGroup direction="horizontal">
-                <ResizablePanel 
-                defaultSize={45}
-                minSize={20}
-                className="flex flex-col min-h-0"
-                >   <Suspense fallback={<p>Loading project...</p>}>
-                        <ProjectHeader projectId={projectId} />
-                    </Suspense>
-                    <Suspense fallback={<p>Loading Project...</p>}>
-                    <MessagesContainer projectId={projectId} 
-                    activeFragment={activeFragment}
-                    setActiveFragment={setActiveFragment}
-                    />
-                    </Suspense>
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel 
-                defaultSize={55}
-                minSize={50}
-                >
-                    Preview Panel
-                </ResizablePanel>
-            </ResizablePanelGroup>  
-        </div>
-    );
-}    
+  return (
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={45}
+          minSize={20}
+          className="flex flex-col min-h-0"
+        >
+          {" "}
+          <Suspense fallback={<p>Loading project...</p>}>
+            <ProjectHeader projectId={projectId} />
+          </Suspense>
+          <Suspense fallback={<p>Loading Project...</p>}>
+            <MessagesContainer
+              projectId={projectId}
+              activeFragment={activeFragment}
+              setActiveFragment={setActiveFragment}
+            />
+          </Suspense>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={55} minSize={50}>
+          {!!activeFragment && <FragmentWeb data={activeFragment} />}
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
