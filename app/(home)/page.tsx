b2cb2b2430191abc598acaeee32f3787c9c39120
mohamed atia <mohamedd.attia11@gmail.com>
2026-01-27 18:13:27 +0200

    remove test from code

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,8 @@
 "use client" 
 import { ProjectForm } from "@/modules/home/ui/Components/project-form";
 import { ProjectList } from "@/modules/home/ui/Components/project-list";
-import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 const Page = () => {
-  const {isSignedIn} =useUser() 
-  console.log(isSignedIn);
   return (
     <div className="flex flex-col max-w-5xl mx-auto w-full">
       <section className="space-y-6 py-[16vh] 2xl:py-48">
