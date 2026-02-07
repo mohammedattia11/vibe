086cefd22495209e754b29f78393ae11d4798b7e
mohamed atia <mohamedd.attia11@gmail.com>
2026-02-08 00:43:12 +0200

    feat:applying feature arch on messages feature

--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,7 +1,7 @@
 import { createTRPCRouter } from "../init";
 import { projectsRouter } from "@/modules/projects/server/procedures";
 
-import { messagesRouter } from "@/modules/messages/server/procedures";
+import { messagesRouter } from "@/features/messages/trpc/procedures";
 import { usageRouter } from "@/modules/usage/server/procedures";
 
 export const appRouter = createTRPCRouter({
